55a32847efbfc8706707bc34893b721171789378
Jhonny Mezzari <jhonnybif@gmail.com>
2023-05-08 18:09:54 -0300

    Js pronto responsividade Quase

--- scrpit.js
+++ scrpit.js
@@ -1,3 +1,11 @@
+let menuIcon =  document.querySelector('#menu-icon');
+let navbar =  document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
@@ -17,5 +25,8 @@ window.onscroll = () => {
   });
 
   let header = document.querySelector('header');
-  header.classList.toogle('sticky', window.scrollY > 100)
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
