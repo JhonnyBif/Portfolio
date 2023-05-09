a5ec14b883d91908bb2848bd8d4b449daef3b9c1
Jhonny Mezzari <jhonnybif@gmail.com>
2023-05-09 15:06:41 -0300

    Finalizado, faltando preencher

--- scrpit.js
+++ scrpit.js
@@ -30,3 +30,22 @@ window.onscroll = () => {
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
+
+ScrollReveal({ 
+  reset: true,
+  distance: '80px',
+  duration: 1000,
+  delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+
+const typed = new Typed('.multiple-text',{
+  strings: ['Fullstack Develope', 'FrontEnd Developer', 'BackEnd Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
